28d4a92bf73ebad3a2be9317d396f594796b9b44
DomainsHax <DomainsHax@gmail.com>
2016-11-16 22:33:24 +0100

    Games List Updated (Play)

--- opencodegames.cpp
+++ opencodegames.cpp
@@ -2,6 +2,10 @@
 #include "ui_opencodegames.h"
 
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QScrollArea>
+#include <QGridLayout>
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -16,6 +20,7 @@ OpenCodeGames::OpenCodeGames(QWidget *parent) : QWidget(parent), ui(new Ui::Open
     {
         addr = ip.addresses().first();
         PlayerStatsConnect();
+        QTimer::singleShot(500,this,SLOT(GamesListConnect()));
     }
 }
 
@@ -28,17 +33,37 @@ void OpenCodeGames::Connection(std::string text_string)
     connect(&connection_info, SIGNAL(readyRead()), this, SLOT(startRead()));
 }
 
+void OpenCodeGames::GamesListConnect() { Connection(std::string("GAMES " + std::to_string(AfterID))); }
 void OpenCodeGames::PlayerStatsConnect(){ Connection(std::string("INFO_USERNAME " + token.toStdString())); }
 
 void OpenCodeGames::startRead()
 {
     connection_info.waitForReadyRead();
     const std::string status_s = QString::fromStdString(QByteArray(connection_info.read(800)).toStdString()).toStdString();
+    std::regex regex_games("^GAMES ([0-9]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9.]+) ([0-9]+) ([a-zA-Z0-9.]+) ([0-9]+)$");
     std::regex regex_info_results("^INFO_RESULTS ([a-zA-Z0-9.-_-]+) ([a-zA-Z0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)$");
     std::regex regex_set_username("^SET_USERNAME ([a-zA-Z0-9.-_-]+)$");
     std::smatch match;
 
-    if(std::regex_match(status_s, regex_info_results) && std::regex_search(status_s.begin(), status_s.end(), match, regex_info_results))
+    if (regex_match(status_s, regex_games) && regex_search(status_s.begin(), status_s.end(), match, regex_games))
+    {
+        int ID = atoi(std::string(match[1]).c_str());
+        bool After;
+
+        if(std::string(match[6]) == "0") After = false;
+        else After = true;
+
+        AfterID = ID + 1;
+        MapGames[ID].ID = ID;
+        MapGames[ID].Name = QString::fromStdString(std::string(match[2]));
+        MapGames[ID].Server = QString::fromStdString(match[3]);
+        MapGames[ID].Port = atoi(std::string(match[4]).c_str());
+        MapGames[ID].Version = QString::fromStdString(match[5]);
+
+        if(After == true) QTimer::singleShot(200,this,SLOT(GamesListConnect()));
+        else Games_List();
+    }
+    else if(std::regex_match(status_s, regex_info_results) && std::regex_search(status_s.begin(), status_s.end(), match, regex_info_results))
     {
         username = QString::fromStdString(match[1]);
         token = QString::fromStdString(match[2]);
@@ -60,18 +85,16 @@ void OpenCodeGames::startRead()
         username = QString::fromStdString(std::string(match[1]));
         ui->UserNameValue->setText(username);
         ui->SUsernameValue->setText(username);
-        qDebug() << "Username is changed\n";
+        qDebug() << "Username is changed";
     }
-    else if(status_s == "USERNAME_ALREADY_EXISTS") qDebug() << "Username already exists\n";
-    else if(status_s == "USERNAME_ALREADY_SETTED") qDebug() << "Username already setted\n";
-    else if(status_s == "NO-CHANGE_USERNAME") qDebug() << "Username isn't changed (chars incorrect)\n";
-
-    else if(status_s == "SET_PASSWORD") qDebug() << "Password is changed\n";
-    else if(status_s == "PASSWORD_ALREADY_SETTED") qDebug() << "Password is already setted\n";
-    else if(status_s == "SET_PASSWORD_INCORRECT") qDebug() << "Last Password isn't correct\n";
-    else if(status_s == "NO-CHANGE_PASSWORD") qDebug() << "Password isn't changed (chars incorrect)\n";
-
-    else if(status_s == "TOKEN_DOESNT_EXISTS") qDebug() << "token doesn't exists\n";
+    else if(status_s == "USERNAME_ALREADY_EXISTS") qDebug() << "Username already exists";
+    else if(status_s == "USERNAME_ALREADY_SETTED") qDebug() << "Username already setted";
+    else if(status_s == "NO-CHANGE_USERNAME") qDebug() << "Username isn't changed (chars incorrect)";
+    else if(status_s == "SET_PASSWORD") qDebug() << "Password is changed";
+    else if(status_s == "PASSWORD_ALREADY_SETTED") qDebug() << "Password is already setted";
+    else if(status_s == "SET_PASSWORD_INCORRECT") qDebug() << "Last Password isn't correct";
+    else if(status_s == "NO-CHANGE_PASSWORD") qDebug() << "Password isn't changed (chars incorrect)";
+    else if(status_s == "TOKEN_DOESNT_EXISTS") qDebug() << "token doesn't exists";
 }
 
 void OpenCodeGames::CenterDisp(int disposition)
@@ -90,39 +113,99 @@ void OpenCodeGames::on_SettingsButton_clicked(){ CenterDisp(2); }
 void OpenCodeGames::on_PlayButton_clicked(){ CenterDisp(3); }
 void OpenCodeGames::on_QuitButton_clicked(){ close(); }
 
+void OpenCodeGames::Games_List()
+{
+    QVBoxLayout *GamesScrollLayout = new QVBoxLayout;
+    int max = MapGames.size();
+
+    for(int i = 1; i < max + 1; i++)
+    {
+        QWidget *GameStatus = new QWidget;
+        QWidget *pic = new QWidget;
+        QLabel *Name = new QLabel(MapGames[i].Name);
+        QGridLayout *GameStatusLayout = new QGridLayout;
+        QPushButton *Status = new QPushButton;
+
+        QString Version = MapGames[i].Version;
+        QString Vers = NULL;
+        QString dir = "/root/OpenCodeGames/Games/" + MapGames[i].Name;
+        QDir* DIRECTORY = new QDir(dir);
+        QFile* VERSION = new QFile(dir + "/VERSION.txt");
+        QFile* EXEC = new QFile(dir + "/" + MapGames[i].Name);
+
+        if(DIRECTORY->exists())
+        {
+            if(VERSION->exists())
+            {
+                VERSION->open(QIODevice::ReadOnly);
+                Vers = VERSION->readAll();
+            }
+        }
+
+        GameStatus->setStyleSheet("background-color:grey;");
+        if(max <= 4) GameStatus->setFixedSize(306, 83);
+        else GameStatus->setFixedSize(292, 84);
+
+        pic->setFixedSize(65, 65);
+        pic->setStyleSheet("background-color:blue;");
+
+        Name->setFixedSize(150, 15);
+        Name->setAlignment(Qt::AlignCenter);
+
+        Status->setFixedSize(45, 30);
+
+        GameStatusLayout->addWidget(pic, 0, 0, 0, 0, Qt::AlignLeft);
+        GameStatusLayout->addWidget(Name, 0, 2, 0, 4, Qt::AlignTop);
+
+        int flag = 0;
+
+        if(Vers == Version && DIRECTORY->exists() && VERSION->exists() && EXEC->exists())
+            Status->setText("Play");
+        else if(!DIRECTORY->exists() && !EXEC->exists() && !VERSION->exists())
+        {
+            Status->setText("Install");
+            flag = 1;
+        }
+        else if((Vers != Version or !EXEC->exists() or !VERSION->exists()) && flag == 0)
+            Status->setText("Update");
+
+        GameStatusLayout->addWidget(Status, 1, 5, 1, 1, Qt::AlignBottom);
+        GamesScrollLayout->addWidget(GameStatus);
+        GameStatus->setLayout(GameStatusLayout);
+    }
+
+    ui->PlayScrollAreaWidget->setLayout(GamesScrollLayout);
+}
+
 void OpenCodeGames::on_SSaveButton_clicked()
 {
-    if((username != ui->SUsernameValue->text())   &&   (!ui->SPasswordValue->text().isEmpty() && !ui->SNewPasswordValue->text().isEmpty() && !ui->SRetypeNewPasswordValue->text().isEmpty()))
+    int settings = 0;
+    if(username != ui->SUsernameValue->text() && !ui->SUsernameValue->text().isEmpty())
+        settings += 1;
+    if(ui->SNewPasswordValue->text() == ui->SRetypeNewPasswordValue->text() && !ui->SPasswordValue->text().isEmpty() && !ui->SNewPasswordValue->text().isEmpty() && !ui->SRetypeNewPasswordValue->text().isEmpty())
+        settings += 2;
+
+    if(settings == 1) SettingsUsername();
+    else if(settings == 2) SettingsPassword();
+    else if(settings == 3)
     {
         SettingsUsername();
         QTimer::singleShot(500,this,SLOT(SettingsPassword()));
     }
-    else if(username != ui->SUsernameValue->text())
-        SettingsUsername();
-    else if(!ui->SPasswordValue->text().isEmpty() && !ui->SNewPasswordValue->text().isEmpty() && !ui->SRetypeNewPasswordValue->text().isEmpty())
-        SettingsPassword();
 }
 
 void OpenCodeGames::SettingsUsername()
 {
-    if(!ui->SUsernameValue->text().isEmpty())
-    {
-        if(ui->SUsernameValue->text().length() >= 3 && ui->SUsernameValue->text().length() <= 16)
-            Connection(std::string("SETTINGS_USERNAME " + token.toStdString() + " " + ui->SUsernameValue->text().toStdString()));
-        else if(ui->SUsernameValue->text().length() <= 3) qDebug() << "Username < 3 chars";
-        else if(ui->SUsernameValue->text().length() >= 16) qDebug() << "Username > 16 chars";
-    }
+    if(ui->SUsernameValue->text().length() >= 3 && ui->SUsernameValue->text().length() <= 16)
+        Connection(std::string("SETTINGS_USERNAME " + token.toStdString() + " " + ui->SUsernameValue->text().toStdString()));
+    else if(ui->SUsernameValue->text().length() <= 3) qDebug() << "Username < 3 chars";
 }
 
 void OpenCodeGames::SettingsPassword()
 {
-    if(ui->SNewPasswordValue->text() == ui->SRetypeNewPasswordValue->text())
-    {
-        if((ui->SPasswordValue->text().length() >= 6 && ui->SPasswordValue->text().length() <= 32) && (ui->SNewPasswordValue->text().length() >= 5 && ui->SNewPasswordValue->text().length() <= 32) && (ui->SRetypeNewPasswordValue->text().length() >= 5 && ui->SRetypeNewPasswordValue->text().length() <= 32))
-            Connection(std::string("SETTINGS_PASSWORD " + token.toStdString() + " " + ui->SPasswordValue->text().toStdString() + " " + ui->SNewPasswordValue->text().toStdString()));
-        else if(ui->SNewPasswordValue->text().length() <= 6) qDebug() << "Password < 6 chars";
-        else if(ui->SNewPasswordValue->text().length() >= 32) qDebug() << "Password > 32 chars";
-    }
+    if((ui->SPasswordValue->text().length() >= 6 && ui->SPasswordValue->text().length() <= 32) && (ui->SNewPasswordValue->text().length() >= 6 && ui->SNewPasswordValue->text().length() <= 32) && (ui->SRetypeNewPasswordValue->text().length() >= 6 && ui->SRetypeNewPasswordValue->text().length() <= 32))
+        Connection(std::string("SETTINGS_PASSWORD " + token.toStdString() + " " + ui->SPasswordValue->text().toStdString() + " " + ui->SNewPasswordValue->text().toStdString()));
+    else if(ui->SNewPasswordValue->text().length() <= 6) qDebug() << "Password < 6 chars";
 }
 
 void OpenCodeGames::on_SCancelButton_clicked()
--- opencodegames.h
+++ opencodegames.h
@@ -9,6 +9,15 @@ namespace Ui {
 class OpenCodeGames;
 }
 
+struct StructGames
+{
+    int ID;
+    QString Name;
+    QString Server;
+    int Port;
+    QString Version;
+};
+
 class OpenCodeGames : public QWidget
 {
     Q_OBJECT
@@ -17,11 +26,12 @@ public:
     explicit OpenCodeGames(QWidget *parent = 0);
     ~OpenCodeGames();
     void Connection(std::string text_string);
-    void PlayerStatsConnect();
     void CenterDisp(int disposition);
 
 private slots:
     void startRead();
+    void GamesListConnect();
+    void PlayerStatsConnect();
     void on_HomeButton_clicked();
     void on_SettingsButton_clicked();
     void on_PlayButton_clicked();
@@ -30,6 +40,7 @@ private slots:
     void on_SCancelButton_clicked();
     void SettingsUsername();
     void SettingsPassword();
+    void Games_List();
 
 private:
     Ui::OpenCodeGames *ui;
@@ -37,6 +48,8 @@ private:
     QTcpSocket connection_info;
     QString username, token = "63253fa398dfa1e0c67d04e512f9d79f953bf074cd592dbcf9692a5e71e9ac0c";
     int LevelValue, ProgressValue, ProgressEnd, ProgressPercentual, Progress;
+    int AfterID = 1;
+    std::map<int, StructGames> MapGames;
 };
 
 #endif // OPENCODEGAMES_H
